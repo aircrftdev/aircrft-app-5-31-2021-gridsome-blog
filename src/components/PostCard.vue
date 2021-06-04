5a19e5d61c3385225d607d0814e924c2bd535e89
Dev <cf@CFs-iMac.local>
2021-06-03 20:48:17 -0700

    Commit

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -17,9 +17,9 @@
            <br>
            <br>
 
-        <a href="https://gumroad.com/l/zwIVA" target=“_blank” class="button primary button--large"> 
+        <!-- <a href="https://gumroad.com/l/zwIVA" target=“_blank” class="button primary button--large"> 
          Buy Now | Blog-in-a-Box  
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"/><polyline points="12 5 19 12 12 19"/></svg></a> 
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"/><polyline points="12 5 19 12 12 19"/></svg></a> -->
 
       </div>
 
